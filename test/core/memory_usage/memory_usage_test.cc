e658e3569d061afd9e9f5e086d3623c74bf7fc87
rishesh007 <40896662+rishesh007@users.noreply.github.com>
2026-04-07 09:52:46 +0000

    Automated change: Fix sanity tests

--- test/core/memory_usage/memory_usage_test.cc
+++ test/core/memory_usage/memory_usage_test.cc
@@ -309,8 +309,10 @@ int main(int argc, char** argv) {
       {"minstack", {/*client=*/{"--minstack"}, /*server=*/{"--minstack"}}},
       {"insecure", {{}, {}}},
       {"chaotic_good", {{"--chaotic_good"}, {"--chaotic_good"}}}};
-  if (absl::GetFlag(FLAGS_use_xds) && absl::GetFlag(FLAGS_scenario_config) == "chaotic_good") {
-    LOG(INFO) << "Skipping chaotic_good scenario because xDS is enabled and the implementation is experimental/incompatible.";
+  if (absl::GetFlag(FLAGS_use_xds) &&
+      absl::GetFlag(FLAGS_scenario_config) == "chaotic_good") {
+    LOG(INFO) << "Skipping chaotic_good scenario because xDS is enabled and "
+                 "the implementation is experimental/incompatible.";
     return 0;
   }
   auto it_scenario = scenarios.find(absl::GetFlag(FLAGS_scenario_config));
